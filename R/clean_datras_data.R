31e04d4af8d86c2512b23b878e99f918204268e5
Rems <remi.vergnon@free.fr>
2016-04-13 15:31:41 +0100

    first version of R package Rmerp

---
+++ R/clean_datras_data.R
@@ -0,0 +1,38 @@
+#' Function to clean DATRAS survey data
+#'
+#' This function cleans up datras data uploaded into R, for example using the get_datras_data function.
+#' DATRAS data is typically messy and gathers information collected using different sampling
+#' protocols (gear type for instance). Analysing data across surveys without this cleaning step
+#' is therefore unadvisable. \code{clean_data_datras} conserves the bulk
+#' of the upoloaded data but exclude problematic observations.
+#' 
+#' The majority of surveys on DATRAS are IBTS surveys. There are consistent in sampling protocol
+#' from 1980 onwards. Among the other surveys only the English Beam Trawl survey have a consistent
+#' sampling protocol through time and other sources of information are excluded.
+#' Other cleaning rules include: gear consistency (GOV if IBTS survey or 4-meter beam trawl if
+#' English Trawl survey); haul validity; haul duration (hauls are normally 30 minutes long but
+#' can vary, we exclude overly short or long hauls).
+#' 
+#' @param survey is an R data.frame 
+#' @param beam is a boolean argument. Default value is FALSE. If TRUE, the data comes from 
+#' a beam trawl survey, rather than from the Internation Bottom Trawl Survey (IBTS).
+#' 
+#' @examples
+#' data(datras_surveys)
+#' mysurvey <- datras_surveys$survey[1]
+#' myear <- datras_surveys$year[1]
+#' myquarter <- datras_surveys$quarter[1]
+#' mysurvey <- get_datras_data("HH", mysurvey, myear, myquarter)
+#' mycleansurvey <- clean_datras_data(mysurvey, beam = F)
+
+# some cleaning steps - optional but recommanded
+clean_datras_data <- function(survey, beam = F){
+  if(beam == F) condition1 <- survey$Gear == "GOV"
+  else {condition1 <- survey$Gear == "BT4A" & survey$Country == "ENG"}
+  # check for duration
+  survey$HaulDur <- as.numeric(as.character(survey$HaulDur))
+  condition2 <- survey$HaulDur > 27 & survey$HaulDur < 33  
+  # check for validity
+  condition3 <- survey$HaulVal == "V"
+  survey[condition1 & condition2 & condition3, ]
+}
---
+++ R/get_datras_data.R
@@ -0,0 +1,37 @@
+#' Function to upload DATRAS survey data into R
+#'
+#' This function uploads data from one quarter and one year at a time, for a given survey.
+#' Several other tools are available to the R user to perform this task, including
+#' the DATRAS R package. A more sophisticated approach has been developed recently by Scott Large
+#' and is available at http://www.ices.dk/marine-data/Documents/DATRAS/DATRAS_R.pdf.
+#' Currently this code only exists as a pdf and would require a complete
+#' re-write as an R script.
+#' 
+#' \code{get_datras_data} achieves the same result as Scott Large's code,
+#' but leaves the task of aggregating surveys (if multiple surveys are uploaded) to the user. 
+#' Uploading the data is a slow process and partly depends on internet speed. Parallelising 
+#' the code as done by Scott Large will potentially speed things up, although available
+#' bandwith will always be a problem. Note that a list of available surveys, years and quarters is stored 
+#' in the data.frame "datras_surveys", which can be looked up using data(datras_surveys)
+#' 
+#' @param whatinfo is a character variable "HH", "HL" or "CA".
+#' "HH" is haul information, "HL" is haul contents by size, and "CA" is haul contents by age
+#' @param survey is a character variable matching the survey code wanted
+#' @param year is a character variable for the year wanted
+#' @param quarter is a character variable for the quarter wanted
+#' 
+#' 
+#' @examples
+#' data(datras_surveys)
+#' mysurvey <- datras_surveys$survey[1]
+#' myear <- datras_surveys$year[1]
+#' myquarter <- datras_surveys$quarter[1]
+#' get_datras_data("HH", mysurvey, myear, myquarter)
+
+get_datras_data <- function(whatinfo, survey, year, quarter){
+  whichAPI <- switch(whatinfo,HH = "getHHdata",HL = "getHLdata",CA = "getCAdata")
+  myDATRASurl <- paste("https://datras.ices.dk/WebServices/DATRASWebService.asmx/",whichAPI,"?survey=",survey,"&year=",year,"&quarter=",quarter,sep="")
+  myxml <- xmlTreeParse(getURL(myDATRASurl))
+  xmltop = xmlRoot(myxml)
+  data.frame(t(xmlSApply(xmltop, function(x) xmlSApply(x, xmlValue))),row.names=NULL)
+}
