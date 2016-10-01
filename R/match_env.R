b2319dca31b96a2ea1a32c92d64cc6a19ec1447c
Rems <remi.vergnon@free.fr>
2016-10-01 23:10:24 +0100

    new documentation

--- R/match_env.R
+++ R/match_env.R
@@ -1,25 +1,21 @@
 #' Match biotic and abiotic data in space
 #'
-#' The function takes the meiofauna database MANUELA
-#' and use dplyr to merge its tables into a data.frame and/or csv file.
-#' There are 14 tables in total. the first 4 concern abiotic measurements 
-#' 5 and 6 have biotic data, with names, count, length, width and biomass
+#' The function takes two sets of geolocated data (typically one biotic
+#' and one abiotic) and match their locations to combine the two.
 
-#' do I need to disconnect the link between R and the database?
-
-#' \code{match_env} merges 4 of the tables together.
-#' The database itself is on Strathcloud in module1/data
-#' and needs to be downloaded on the user's computer.
+#' \code{match_env} returns the value of the environmental variable
+#' for the location in the abiotic set of coordinates that is closest to the 
+#' location for the biotic record. If the vector of values for 
+#' the abiotic variable is not provided, the function simply returns the
+#' index of the closest location in the vector of coordinates. 
 #' 
-#' @param env_lon is the longitude of the abiotic variable
-#' @param env_lat is the latitude of the abiotic variable
+#' @param env_lon is a vector of longitudes for the abiotic variable
+#' @param env_lat is a vector of latitude for the abiotic variable
 #' @param bio_lon is the longitude of the biotic variable
 #' @param bio_lat is the latitude of the biotic variable
-#' @param env_variable is the value of the abiotic variable
+#' @param env_variable is an optional vector containing the value of the abiotic variable
 #' 
-#' @examples
-#' data(datras_surveys)
-
+#' 
 match_env <- function(env_lon, env_lat, bio_lon, bio_lat, env_variable = NULL){
   alldist <- sapply(c(1:length(env_lon)), function(i) dist(rbind(c(env_lon[i], env_lat[i]), c(bio_lon, bio_lat))))
   if(is.null(env_variable)) return(which.min(alldist))
