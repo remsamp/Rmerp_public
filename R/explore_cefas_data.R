33e789d16535c1aab7caea7e286e1a6151f7af3d
Rems <remi.vergnon@free.fr>
2016-11-29 09:52:22 +0000

    new function to search cefas data sources by keyword

---
+++ R/explore_cefas_data.R
@@ -0,0 +1,23 @@
+#' Explore CEFAS data sources availabe from CEFAS API
+#'
+#' The function returns all recordset names matching 
+#' a user-provided keyword 
+#'
+#' \code{explore_cefas_data} takes two arguments. The first
+#' one list_sources is a list of names to look into. In the context 
+#' of the CEFAS API this will typically be the "Name" columns of the
+#' data returned by the function get_cefas_datasets(). The 
+#' second argument keyword is a keyword provided by the user. 
+#' The inputed keyword is not case sensitive. 
+#'  
+#' @return a subset data.frame containing all data sources whose
+#' names match the provided keyword.
+#' 
+#' @examples
+#' mydata <- get_cefas_datasets()
+#' explore_cefas_data(list_sources = mydata$list_data_sets$Name, keyword = "Plankton")
+#' 
+#' 
+explore_cefas_data <- function(list_sources = list_data_sets$Name, keyword){
+  dat[grep(dat, pattern = keyword, ignore.case = T)]
+}
